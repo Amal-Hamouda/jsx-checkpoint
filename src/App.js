8b0b9d8f02fe57c042cb601b2a5e83906050f249
Amal-Hamouda <82983648+Amal-Hamouda@users.noreply.github.com>
2021-06-08 18:03:19 +0100

    Amal first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css'
+import React from 'react';
+import myImg from './imageInSrc.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <React.Fragment>
+  <div className="All" alt='1'>
+
+<h1 className="title red">Amal H</h1>
+
+<br />
+
+<img src={myImg} alt='2'/>
+
+<br />
+
+<img src="imageInPublic.jpg" alt='3'/>
+
+<video width="320" height="240" controls >
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
+
+</div>
+ 
+</React.Fragment>
   );
 }
 
